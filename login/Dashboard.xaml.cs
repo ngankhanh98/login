8fdfbab32159a609597ecf8d5b413e01b372daaf
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2018-11-22 00:12:50 +0700

    Binding price completed

--- login/Dashboard.xaml.cs
+++ login/Dashboard.xaml.cs
@@ -41,14 +41,14 @@ namespace login
             list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/44.500 - Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa.png" });
             list.Add(new Product() { Name = "Đời Ngắn Đừng Ngủ Dài - Robin Sharma", Price = "47.000", Path = "../images/47.000 - Đời Ngắn Đừng Ngủ Dài - Robin Sharma.png" });
             list.Add(new Product() { Name = "Nghĩ Đơn Giản, Sống Đơn Thuần - Tolly Burkan", Price = "47.000", Path = "../images/47.000 - Nghĩ Đơn Giản, Sống Đơn Thuần - Tolly Burkan.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/48.000 - Đọc Vị Bất Kỳ Ai (Tái Bản) - TS. David J. Lieberman.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/49.000 - Tuổi Trẻ Đáng Giá Bao Nhiêu Rosie Nguyễn.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/52.000 - Đắc Nhân Tâm (Khổ Lớn) - Dale Carnegie.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/55.000 - Nhà Giả Kim - Paulo Coelho.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/60.500 - Nghệ Thuật Tinh Tế Của Việc Đếch Quan Tâm - Mark Manson.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/62.000 - Hành Trình Về Phương Đông (Tái Bản) - Baird T. Spalding.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/76.000 - Khi Hơi Thở Hóa Thinh Không - Paul Kalanithi.png" });
-            list.Add(new Product() { Name = "Mình Là Cá, Việc Của Mình Là Bơi - Takeshi Furukawa", Price = "44.500", Path = "../images/81.000 - Giết Con Chim Nhại (Tái Bản 2016) - Harper Lee.png" });
+            list.Add(new Product() { Name = "Đọc Vị Bất Kỳ Ai (Tái Bản) - TS. David J. Lieberman", Price = "48.000", Path = "../images/48.000 - Đọc Vị Bất Kỳ Ai (Tái Bản) - TS. David J. Lieberman.png" });
+            list.Add(new Product() { Name = "Tuổi Trẻ Đáng Giá Bao Nhiêu - Rosie Nguyễn", Price = "49.000", Path = "../images/49.000 - Tuổi Trẻ Đáng Giá Bao Nhiêu - Rosie Nguyễn.png" });
+            list.Add(new Product() { Name = "Đắc Nhân Tâm (Khổ Lớn) - Dale Carnegie", Price = "52.000", Path = "../images/52.000 - Đắc Nhân Tâm (Khổ Lớn) - Dale Carnegie.png" });
+            list.Add(new Product() { Name = "Nhà Giả Kim - Paulo Coelho", Price = "55.000", Path = "../images/55.000 - Nhà Giả Kim - Paulo Coelho.png" });
+            list.Add(new Product() { Name = "Nghệ Thuật Tinh Tế Của Việc Đếch Quan Tâm - Mark Manson", Price = "60.500", Path = "../images/60.500 - Nghệ Thuật Tinh Tế Của Việc Đếch Quan Tâm - Mark Manson.png" });
+            list.Add(new Product() { Name = "Hành Trình Về Phương Đông (Tái Bản) - Baird T. Spalding", Price = "62.000", Path = "../images/62.000 - Hành Trình Về Phương Đông (Tái Bản) - Baird T. Spalding.png" });
+            list.Add(new Product() { Name = "Khi Hơi Thở Hóa Thinh Không - Paul Kalanithi", Price = "76.000", Path = "../images/76.000 - Khi Hơi Thở Hóa Thinh Không - Paul Kalanithi.png" });
+            list.Add(new Product() { Name = "Giết Con Chim Nhại (Tái Bản 2016) - Harper Lee", Price = "81.000", Path = "../images/81.000 - Giết Con Chim Nhại (Tái Bản 2016) - Harper Lee.png" });
 
 
             icProducts.ItemsSource = list;
